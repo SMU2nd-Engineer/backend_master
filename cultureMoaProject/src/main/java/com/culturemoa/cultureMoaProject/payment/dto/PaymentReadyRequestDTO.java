64f3da772cd9099f8d4c279b8387fec41af2434b
cky0902 <cky990902@gmail.com>
2025-05-12 16:00:48 +0900

    Create PaymentReadyRequestDTO.java

---
+++ cultureMoaProject/src/main/java/com/culturemoa/cultureMoaProject/payment/dto/PaymentReadyRequestDTO.java
@@ -0,0 +1,7 @@
+package com.culturemoa.cultureMoaProject.payment.dto;
+
+public class PaymentReadyRequestDTO {
+    private String itemName;
+    private  int amount;
+    private  String userId;
+}
