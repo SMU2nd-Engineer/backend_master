5b2007118b29561c356d398e22d0c35fe6fcf7a7
yellow-pang <troh0322@naver.com>
2025-05-13 17:24:43 +0900

    Modify xml and modify mypage DAO
    Modify xml and modify mypage DAO

--- cultureMoaProject/src/main/java/com/culturemoa/cultureMoaProject/user/repository/MyPageDAO.java
+++ cultureMoaProject/src/main/java/com/culturemoa/cultureMoaProject/user/repository/MyPageDAO.java
@@ -1,5 +1,6 @@
 package com.culturemoa.cultureMoaProject.user.repository;
 
+import com.culturemoa.cultureMoaProject.user.dto.MyPageCheckSocialDTO;
 import com.culturemoa.cultureMoaProject.user.dto.MyPagePasswordCheckDTO;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,5 +17,8 @@ public class MyPageDAO {
 
     public MyPagePasswordCheckDTO getPassword(String userId) {
         return sqlSessionTemplate.selectOne("myPageMapper.findPasswordByTokenId", userId);
+    }
+    public MyPageCheckSocialDTO getSocialLogin(String tokenUserId) {
+        return sqlSessionTemplate.selectOne("myPageMapper.findSocialLoginByTokenId", tokenUserId);
     }
 }
