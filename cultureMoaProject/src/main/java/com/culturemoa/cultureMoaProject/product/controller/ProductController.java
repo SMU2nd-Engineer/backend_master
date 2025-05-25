10fd7ebe57f7688fd4f2d143eadb7b96bde76fa4
kwsubin <kwsubin@gmail.com>
2025-05-25 15:54:40 +0900

    0524 imageupload
    다중 이미지 업로드

--- cultureMoaProject/src/main/java/com/culturemoa/cultureMoaProject/product/controller/ProductController.java
+++ cultureMoaProject/src/main/java/com/culturemoa/cultureMoaProject/product/controller/ProductController.java
@@ -1,11 +1,14 @@
 package com.culturemoa.cultureMoaProject.product.controller;
 
 import com.culturemoa.cultureMoaProject.product.dto.ProductDTO;
+import com.culturemoa.cultureMoaProject.product.dto.ProductImageDTO;
 import com.culturemoa.cultureMoaProject.product.dto.ProductSearchDTO;
 import com.culturemoa.cultureMoaProject.product.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -29,18 +32,42 @@ public class ProductController {
         return productService.getProductByIdx(idx);
     }
 
+
     @PostMapping("/upload")
-    public ProductDTO ProductUpload (@RequestBody ProductDTO productDTO) {
-//        if(imageUrl == null || imageUrl.isEmpty()) {
-//            System.out.println("이미지가 업로드 되지 않았습니다.");
-//        } else {
-//            System.out.println("이미지가 저장되었습니다.");
-//        }
-        System.out.println("이미지가 저장되었습니다.");
-        productDTO.setFlag(false);
-        productService.insertProduct(productDTO);
-        return productDTO;
+    public ProductDTO ProductUpload (@RequestPart("product") ProductDTO productDTO, @RequestPart("files") List<MultipartFile> files) {
+        System.out.println("업로드 실행");
+        try {
+            String uploadDir = "C:/market_image/";
+            List<ProductImageDTO> imageList = new ArrayList<>();
+
+            for (int i = 0; i< files.size(); i++) {
+                MultipartFile file = files.get(i);
+                if (!file.isEmpty()) {
+                    String imageUrl = productService.saveImage(file, uploadDir);
+                    boolean flag = (i == 0 ); // 첫번째 이미지 flag true
+                    imageList.add(new ProductImageDTO(imageUrl, flag));
+                }
+            }
+
+            productDTO.setImageList(imageList);
+
+            if(!imageList.isEmpty()){
+                productDTO.setImageUrl(imageList.get(0).getImageUrl()); // 첫번째 이미지가 썸네일이 되도록
+            }
+            productDTO.setFlag(false);
+            productService.insertProduct(productDTO);
+
+            return productDTO;
+          } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
+
+    @GetMapping("/image/{idx}")
+    public List<ProductImageDTO> imageRead(@PathVariable int product_idx){
+        return productService.imageRead(product_idx);
+    }
+
 
     @PostMapping("/search")
     public List<ProductDTO> searchProducts (@RequestBody ProductSearchDTO searchDTO){
--- cultureMoaProject/src/main/java/com/culturemoa/cultureMoaProject/product/dto/ProductImageDTO.java
+++ cultureMoaProject/src/main/java/com/culturemoa/cultureMoaProject/product/dto/ProductImageDTO.java
@@ -14,3 +14,4 @@ public class ProductImageDTO {
     private boolean flag;
 
 }
+
