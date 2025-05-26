24088fe2da2ec7954e8b5194e75b5d850a2c4d18
zenbms95 <bms051234@gmail.com>
2025-05-26 18:29:27 +0900

    Contents files upload
    25/5/26 2차
    -- 수정
    
    공통사항
    : 게시판 상세페이지 댓글 등록& 목록 가져오기, 삭제 기능 구현
    
    1. contentsMapper
    :  게시글 상세페이지 - 댓글
    1-1. 댓글 등록 쿼리문 수정함
    1-2. 댓글 삭제 새로 작성함(Update set)
    2. ContentsCommentDTO
    : 25/5/25 수정사항
    2-1. cdate 칼럼 삽입
    3. ContentsCommentDAO
    : 25/5/26 수정사항
    3-1.  댓글 삭제 생성
    4. ContentsCommentService
    4-1. 댓글 목록 가져오기 넘길 value 값 수정
    4-2. 댓글 목록에서 댓글 삭제하는 기능 생성
    5. ContentsController
    : 25/5/25 수정사항
    5-1. 댓글 목록 조회 @RequestParam("idx") Long idx로 수정
    6. ContentsCommentInfoDTO
    :
    
    -- 파일 수정
    1. ContentsCommentDeleteInfoDTO
    : 게시판 상세페이지 댓글 삭제 기능 구현 DTO
    idx, edte (삭제되는 날짜를 입력하기 위해 사용)
    
    -- 파일 삭제
    1. ContentsCommentController
    : ContentsController에다가 그냥 다 작성함

--- cultureMoaProject/src/main/java/com/culturemoa/cultureMoaProject/board/dto/ContentsCommentInfoDTO.java
+++ cultureMoaProject/src/main/java/com/culturemoa/cultureMoaProject/board/dto/ContentsCommentInfoDTO.java
@@ -3,6 +3,9 @@ package com.culturemoa.cultureMoaProject.board.dto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDateTime;
 
 @Data
 @NoArgsConstructor
@@ -10,7 +13,15 @@ import lombok.NoArgsConstructor;
 public class ContentsCommentInfoDTO {
 
     // 게시판 댓글 정보
+    private Long user_idx;
     private Long contents_idx;
+    private Long comment_idx; // cotentsMapper의 게시글 상세페이지 댓글의 고유 idx 불러오기 별칭명과 동일하게 설정
     private String text;
+    @DateTimeFormat(pattern = "MM/dd")
+    private LocalDateTime sdate; // 날짜 포맷된 sdate
+
+    // 불러올 User 정보(작성자-회원 닉네임)
+    private String nickname;
+
 
 }
