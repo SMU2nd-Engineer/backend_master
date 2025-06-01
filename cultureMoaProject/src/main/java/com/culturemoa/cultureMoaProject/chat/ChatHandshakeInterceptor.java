dfd66f839e5b237b9df019f16a2183090c416ddf
HKM1998 <86611859+HKM1998@users.noreply.github.com>
2025-06-01 14:59:07 +0900

    250601_websocket_update

--- cultureMoaProject/src/main/java/com/culturemoa/cultureMoaProject/chat/ChatHandshakeInterceptor.java
+++ cultureMoaProject/src/main/java/com/culturemoa/cultureMoaProject/chat/ChatHandshakeInterceptor.java
@@ -1,6 +1,7 @@
 package com.culturemoa.cultureMoaProject.chat;
 
 import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.server.ServerHttpRequest;
 import org.springframework.http.server.ServerHttpResponse;
 import org.springframework.http.server.ServletServerHttpRequest;
@@ -27,12 +28,11 @@ public class ChatHandshakeInterceptor implements HandshakeInterceptor {
 
         // Origin 체크 예시
         String origin = request.getHeaders().getOrigin();
-        return true;
-//        if (origin != null && (origin.equals("http://3.38.104.183") || origin.equals("https://ec2-3-38-104-183.ap-northeast-2.compute.amazonaws.com"))) {
-//            return true;
-//        }
-//        response.setStatusCode(HttpStatus.FORBIDDEN);
-//        return false;
+        if (origin != null && (origin.equals("http://3.38.104.183") || origin.equals("https://ec2-3-38-104-183.ap-northeast-2.compute.amazonaws.com"))) {
+            return true;
+        }
+        response.setStatusCode(HttpStatus.FORBIDDEN);
+        return false;
     }
 
     @Override
